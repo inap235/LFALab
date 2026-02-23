c87ac7862a3f0d3614ac1166a326d9d9bc97187f
Ina Pancenco <ina.pancenco@isa.utm.md>
2026-02-23 22:55:42 +0200

    added type of grammar in grammar class

--- Lab2/grammar.py
+++ Lab2/grammar.py
@@ -1,6 +1,123 @@
+import random
 class Grammar:
-    def __init__(self, VN, VT, S, P):
-        self.VN =VN
+    def __init__(self, VN, VT, P, S):
+        self.VN = VN 
         self.VT = VT
-        self.P = P
-        self.S = S
+        self.P = P #productions list
+        self.S = S #start symbol
+    def generateWords(self, max_steps):
+        current = self.S
+        steps = 0
+
+        while steps < max_steps:
+            pos = -1
+            
+            for i, ch in enumerate(current):
+                if ch in self.VN:
+                    pos = i
+                    break
+            
+            if pos == -1:  
+                return current
+            
+            production = random.choice(self.P[current[pos]])
+            current = current[:pos] + production + current[pos+1:]#combines the produced sequences
+            steps += 1
+        
+        return None  
+    
+    def toAutomaton(self):
+        from automaton import FiniteAutomaton
+        states = set(self.VN)
+        final_state = "X"
+        states.add(final_state)
+        
+        alphabet = set(self.VT)
+        transitions = {}
+        
+        for left, rights in self.P.items():
+            for right in rights:
+                symbol = right[0]
+                # A -> aB
+                if len(right)==2:
+                    next_state = right[1]
+                # A -> a
+                elif len(right) ==1:
+                    next_state = final_state
+                else:
+                    continue
+                
+                key = (left, symbol)
+                if key not in transitions:
+                    transitions[key] = set()
+
+                transitions[key].add(next_state)
+    
+        return FiniteAutomaton(
+            alphabet=alphabet,
+            transitions=transitions,
+            states=states,
+            start=self.S,
+            final={final_state}
+        )
+    def classify_grammar(self):
+        """
+        Returns the Chomsky type:
+        Type 3  Regular
+        Type 2 Context-Free
+        Type 1  Context-Sensitive
+        Type 0 Unrestricted
+        """
+        regular = True
+        for left, rights in self.P.items():
+            if left not in self.VN:
+                regular = False
+                break
+
+            for right in rights:
+                # A -> a
+                if len(right) == 1:
+                    if right[0] not in self.VT:
+                        is_regular = False
+                        break
+
+                # A -> aB
+                elif len(right) == 2:
+                    if right[0] not in self.VT or right[1] not in self.VN:
+                        is_regular = False
+                        break
+                else:
+                    is_regular = False
+                    break
+
+            if not is_regular:
+                break
+        if is_regular:
+            return "Type 3 – Regular grammar"
+
+        #Type 2
+        is_cfl = True
+
+        for left in self.P.keys():
+            if left not in self.VN:
+                is_cfl = False
+                break
+
+        if is_cfl:
+            return "Type 2 – Context-Free grammar"
+
+        # ---------- check Type 1 (context-sensitive) ----------
+        is_csl = True
+
+        for left, rights in self.P.items():
+            for right in rights:
+                if len(right) < len(left):
+                    is_csl = False
+                    break
+            if not is_csl:
+                break
+
+        if is_csl:
+            return "Type 1 – Context-Sensitive grammar"
+
+        return "Type 0 – Unrestricted grammar"
